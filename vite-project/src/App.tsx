3824927921c90ab3f5c8cfb0ce1a225a8b3ba0fe
haikalbintang <mhaikalbintang.work@gmail.com>
2024-09-26 22:04:41 +0700

    Add: Add 3 circle-buttons, a red cross, a margin, and manage state.

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -6,15 +6,23 @@ import {
   // futureDataSeed,
 } from "./data/dummyData";
 import { Item, ListProps } from "./types";
+import { ImCross } from "react-icons/im";
 
 function Navbar({ children, selectedNavLink, setSelectedNavLink }) {
   return (
     <div className="w-full shadow-xl">
       <nav className="mx-auto h-20 max-w-[1366px] flex items-center justify-between">
-        <h1 className="min-w-64 pb-2 mx-4 text-5xl font-bold text-teal-600 flex items-center">
-          To-do App
-        </h1>
-        <ul className="flex gap-11">
+        <div className="flex w-1/3">
+          <h1 className="pb-2 ml-4 text-5xl font-bold text-teal-600 flex items-center">
+            To-do App
+          </h1>
+          <img
+            className="w-16 ml-1"
+            src="/check-icon-big.png"
+            alt="check icon"
+          />
+        </div>
+        <ul className="flex gap-11 w-1/3 items-center justify-center">
           {navLinks.map((navLink) => (
             <li>
               <button
@@ -28,7 +36,7 @@ function Navbar({ children, selectedNavLink, setSelectedNavLink }) {
             </li>
           ))}
         </ul>
-        <div className="min-w-64">{children}</div>
+        <div className="w-1/3 flex justify-end">{children}</div>
       </nav>
     </div>
   );
@@ -44,21 +52,7 @@ const List = ({ title, data, inputValue, onChange, onAddItem }: ListProps) => {
       </div>
       <ol>
         {data.map((data, index) => (
-          <li key={index} className="py-1">
-            <div className="flex items-center justify-between">
-              <p className="border-zinc-700 font-medium">
-                {index + 1}. {data.title}
-              </p>
-              <p>{data.status}</p>
-            </div>
-            <div>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
-              voluptate eveniet eius pariatur repellendus ducimus nulla magni
-              incidunt fugit? Unde pariatur quisquam voluptas, repellat
-              recusandae ad impedit! Error maxime, fuga laborum nisi animi
-              officiis dolorum?
-            </div>
-          </li>
+          <TodoItem key={data.title} data={data} index={index} />
         ))}
       </ol>
       {formIsShown === false ? (
@@ -77,6 +71,52 @@ const List = ({ title, data, inputValue, onChange, onAddItem }: ListProps) => {
     </>
   );
 };
+
+function TodoItem({ data, index }) {
+  const [descIsShown, setDescIsShown] = useState(false);
+
+  function handleToggleDesc() {
+    setDescIsShown((prev) => !prev);
+  }
+  return (
+    <li
+      key={index}
+      className={`${
+        descIsShown ? "bg-emerald-200" : ""
+      } py-1 hover:bg-emerald-200 px-2 my-2 hover:cursor-pointer`}
+    >
+      <div
+        className="flex items-center justify-between"
+        onClick={handleToggleDesc}
+      >
+        <p className="border-zinc-700 font-medium">
+          {index + 1}. {data.title}
+        </p>
+        <div className="flex gap-2">
+          <button className="h-4 w-4 rounded-full bg-emerald-300 border-2 border-zinc-700 hover:border-orange-300 hover:cursor-pointer"></button>
+          <button className="h-4 w-4 rounded-full bg-yellow-300 border-2 border-zinc-700 hover:border-orange-300 hover:cursor-pointer"></button>
+          <button className="h-4 w-4 rounded-full bg-orange-400 border-2 border-zinc-700 hover:border-orange-300 hover:cursor-pointer"></button>
+        </div>
+      </div>
+      {descIsShown && (
+        <div className="relative">
+          <div className="hover:cursor-default">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
+            voluptate eveniet eius pariatur repellendus ducimus nulla magni
+            incidunt fugit? Unde pariatur quisquam voluptas, repellat recusandae
+            ad impedit! Error maxime, fuga laborum nisi animi officiis dolorum?
+          </div>
+          <div
+            onClick={() => setDescIsShown(false)}
+            className="absolute text-xs -right-0.5 bottom-0.5 w-4 h-4 rounded-full text-red-800"
+          >
+            <ImCross />
+          </div>
+        </div>
+      )}
+    </li>
+  );
+}
 
 function App() {
   const [selectedNavLink, setSelectedNavLink] = useState("past");
@@ -101,7 +141,7 @@ function App() {
         selectedNavLink={selectedNavLink}
         setSelectedNavLink={setSelectedNavLink}
       >
-        <button className="bg-fuchsia-900 text-fuchsia-200 py-2 px-6 text-lg rounded-full">
+        <button className="bg-fuchsia-900 text-fuchsia-200 py-2 px-6 text-lg rounded-full mx-10">
           Profile
         </button>
       </Navbar>
