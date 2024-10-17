5624eaaaae1eb98f018d8b0d18fb851008b147d5
haikalbintang <mhaikalbintang.work@gmail.com>
2024-10-17 07:09:17 +0700

    Update: Change title to font-bold form UI in mobile.

--- vite-project/src/components/DisplayTodoItem.tsx
+++ vite-project/src/components/DisplayTodoItem.tsx
@@ -11,7 +11,7 @@ interface DisplayTodoItemProps {
 const DisplayTodoItem = ({ index, data }: DisplayTodoItemProps) => {
   return (
     <div>
-      <h2 className="border-zinc-700 font-semibold">
+      <h2 className="border-zinc-700 font-bold">
         {index + 1}. {data.title}
       </h2>
       <h3 className="text-sm">{data.subtitle}</h3>
