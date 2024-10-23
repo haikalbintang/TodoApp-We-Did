f6338b1f1e2adb3cf3caee26243f5c062a00b7cd
haikalbintang <mhaikalbintang.work@gmail.com>
2024-10-23 14:48:11 +0700

    Deploy.

--- vite-project/src/services/supabase.ts
+++ vite-project/src/services/supabase.ts
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://iydqcxxoakqnzrqnasnn.supabase.co";
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml5ZHFjeHhvYWtxbnpycW5hc25uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc0MzEyNzUsImV4cCI6MjA0MzAwNzI3NX0.C3_K88dEOAteM8yQ1dInt82NJjxTqPpSg8SIdfZ13CE";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
