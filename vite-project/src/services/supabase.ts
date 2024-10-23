287b1ba553d8074f6eb7cb9eaf63419b2f1030a2
haikalbintang <mhaikalbintang.work@gmail.com>
2024-10-23 07:36:59 +0700

    Fix: Fix minor.

--- vite-project/src/services/supabase.ts
+++ vite-project/src/services/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://iydqcxxoakqnzrqnasnn.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml5ZHFjeHhvYWtxbnpycW5hc25uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc0MzEyNzUsImV4cCI6MjA0MzAwNzI3NX0.C3_K88dEOAteM8yQ1dInt82NJjxTqPpSg8SIdfZ13CE";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
